f6196dd9539690eed7b19a015fcbd57dabf92df6
Anthrino <jerinjohn101@gmail.com>
2022-03-26 15:45:56 -0400

    Added home page component and fns

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatasetService } from '../dataset.service';
+import { HeadlineDOM } from '../HeadlineDOM';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  annotateFlag: boolean;
+  validateFlag: boolean;
+  homeFlag: boolean;
+  inputSrNo!: number;
+  headline!: HeadlineDOM;
+
+  constructor(private datasetService: DatasetService) {
+    this.annotateFlag = false;
+    this.validateFlag = false;
+    this.homeFlag = true;
+  }
 
   ngOnInit(): void {
   }
 
+  getHeadlineData(inputSrNo: number): void {
+    this.datasetService.getHeadlineData(inputSrNo).subscribe(
+      (response) => {
+        this.headline = new HeadlineDOM(response);
+      },
+      (error) => {
+        console.log('[Error] Fetching headline data: ' + error);
+        alert('Error fetching headline data');
+        if (inputSrNo > 0) {
+          this.getHeadlineData(inputSrNo - 1);
+        }
+      })
+  }
+
+  btnClick(mode: string): void {
+    if (mode == 'annotate') {
+      this.homeFlag = false;
+      this.validateFlag = false;
+      this.annotateFlag = true;
+      console.log("Switiching visibility for " + mode);
+    } else if (mode == 'validate') {
+      this.homeFlag = false;
+      this.annotateFlag = false;
+      this.validateFlag = true;
+      console.log("Switiching visibility for " + mode);
+    } else if (mode == 'home') {
+      this.homeFlag = true;
+      this.annotateFlag = false;
+      this.validateFlag = false;
+      console.log("Switiching visibility for " + mode);
+    } else if (mode == 'search') {
+      this.getHeadlineData(this.inputSrNo);
+    }
+  }
 }

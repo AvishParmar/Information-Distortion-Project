21f5a225d43c740ecf80c0f93855376138e5e82c
Anthrino <jerinjohn101@gmail.com>
2022-03-26 15:44:11 -0400

    Pushed angular module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AnnotatePageComponent } from './annotate-page/annotate-page.component';
 import { ValidatePageComponent } from './validate-page/validate-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { DatasetService } from './dataset.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +26,19 @@ import { ValidatePageComponent } from './validate-page/validate-page.component';
     ValidatePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatSelectModule,
+    MatListModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DatasetService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
